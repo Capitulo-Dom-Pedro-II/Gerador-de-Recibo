8b762470d0a26754afcf75bba982d637426f99d6
Leonardo Soares <62779334+Leonardo-Soares@users.noreply.github.com>
2022-06-28 21:42:45 -0300

    Opção PDF e Imagem (Print)

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ function gerarRecibo() {
   document.getElementById('recibo_nome').innerHTML = document.getElementById('nome').value;
   document.getElementById('recibo_telefone').innerHTML = document.getElementById('telefone').value;
   document.getElementById('recibo_numero').innerHTML = document.getElementById('numero').value;
+}
+
+function gerarImpressao() {
+  document.getElementById('recibo_nome').innerHTML = document.getElementById('nome').value;
+  document.getElementById('recibo_telefone').innerHTML = document.getElementById('telefone').value;
+  document.getElementById('recibo_numero').innerHTML = document.getElementById('numero').value;
   window.print();
 }
